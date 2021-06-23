bcdb5ebe984a78e66c4d8f45483fc3cacc111ea5
wilber988 <ramosf898@gmail.com>
2021-06-23 15:26:23 -0600

    Caluladora terminda
    Co-Authored-By: null <wilber988@users.noreply.github.com>

--- clases.php
+++ clases.php
@@ -1,14 +1,33 @@
 <?php
-echo "CALCULADORA DE PHP";
-$suma=1;
-$Resta=2;
-$Multiplicacion=3;
-$Divicion=4
-$operacion"";
-echo "Que operacion desea realizar: ";
-echo "1 = Suma";
-echo "2 = Resta";
-echo "3 = Multiplicacion";
-echo "4 = Divicion";
+echo "CALCULADORA DE PHP \n";
+echo "1 = Suma \n";
+echo "2 = Resta \n";
+echo "3 = Multiplicacion \n";
+echo "4 = Divicion \n";
+$i="s";
 //Excelente ahi la llevamos
+while ($i == "s") {
+$operacion=readline("Que operacion desea realizar: \n");
+echo "Muy bien, comenzemos \n";
+$num1=readline("Digite su primero numero: \n");
+$num2=readline("Digite su segundo nuemro: \n");
+
+  if ($operacion==1) {
+    $resultado=$num1+$num2;
+    echo "La suma del los numeros ingresados es: ".$resultado."\n";
+  }
+  if ($operacion==2) {
+    $resultado=$num1-$num2;
+    echo "La Resta del los numeros ingresados es: ".$resultado."\n";
+  }
+  if ($operacion==3) {
+    $resultado=$num1*$num2;
+    echo "La Multiplicacion del los numeros ingresados es: ".$resultado."\n";
+  }
+  if ($operacion==4) {
+    $resultado=$num1/$num2;
+    echo "La Divicion del los numeros ingresados es: ".$resultado."\n";
+  }
+  $i=readline("Desa Continuar s/n \n");
+}
 ?>

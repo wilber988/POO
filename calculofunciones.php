f30b4f0d31f63364812eb6f878fa06414be564ce
wilber988 <ramosf898@gmail.com>
2021-06-24 13:50:03 -0600

    practicando

--- calculofunciones.php
+++ calculofunciones.php
@@ -4,7 +4,7 @@ echo "Suma = 1";
 echo "resta = 2";
 echo "Multiplicacion = 3";
 echo "Divicion = 4";
-$operacion=
+$operacion=readline("Ingrese el numero de la operacion a realizar:");
 function calculo(){
   $suma=$num1+$num2;
 }

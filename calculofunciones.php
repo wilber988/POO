555d8ccd2bbd42f6e68f281d2f6b97a01b874fcf
wilber988 <ramosf898@gmail.com>
2021-06-24 13:49:14 -0600

    comprendiendo funciones

---
+++ calculofunciones.php
@@ -0,0 +1,12 @@
+<?php
+echo "CALCULADORA Funciones";
+echo "Suma = 1";
+echo "resta = 2";
+echo "Multiplicacion = 3";
+echo "Divicion = 4";
+$operacion=
+function calculo(){
+  $suma=$num1+$num2;
+}
+
+?>
